3343ab124ef31ef2904ab00293860df83f061d0e
Todd Pangilinan <pang.todd@gmail.com>
2022-02-04 12:57:11 -0800

    update scripts.js to include branching logic.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("form#dimension").submit(function(event) {
+    event.preventDefault();
+    $("#quiz").hide();
+
+    const sideA = parseFloat($("input#sideOne").val());
+    console.log(sideA)
+    const sideB = parseFloat($("input#sideTwo").val());
+    const sideC = parseFloat($("input#sideThree").val());
+
+    if (sideA+sideB<=sideC || sideB+sideC <=sideA || sideC+sideA<=sideB){
+      $("#nope").show();
+    } else if (sideA === sideB && sideB === sideC) {
+      $("Equilateral").show();
+    } else if (sideA === sideB || sideB === sideC) {
+      $("Isosceles").show();
+    } else {
+      $("Scalene").show();
+    }
+  });
+});
